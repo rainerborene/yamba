a68f85ffd07bffa0d9c24559730eda86884795cd
Rainer Borene <rainerborene@gmail.com>
2015-10-06 14:37:40 -0300

    Initial commit.

---
+++ src/main/kotlin/com/marakana/yamba/StatusActivity.kt
@@ -0,0 +1,13 @@
+package com.marakana.yamba
+
+import android.app.Activity
+import android.os.Bundle
+
+class StatusActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+
+}
